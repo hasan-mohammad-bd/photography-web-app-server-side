fa3e7cfc5420d10eb754714a33642a3f8805d541
hasan-mohammad-bd <hasan.md.east@gmail.com>
2022-06-03 16:19:47 +0600

    adding get request for individual service

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000;
 
 //middleware
@@ -14,7 +14,26 @@ app.use(express.json());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.xecwx.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+const run = async()=>{
+    try{
+        await client.connect()
+        const bookingCollection = client.db('photography').collection('service');
 
+        app.get('/booking/:id', async(req, res)=> {
+            const id = req.params.id
+            const filter = {_id: ObjectId(id)};
+            const service = await bookingCollection.findOne(filter);
+            res.send(service);
+        })
+
+    }
+    finally {
+        await client.close();
+    }
+
+}
+
+run().catch(console.dir);
 
 
 
---
+++ text.js
@@ -0,0 +1,20 @@
+booking=[
+    {
+        name: 'Fashion Shotting',
+        price: 3000,
+        description: 'I am working with fashion photography for so long. Have vast experience. you can choose this package if you need any fashion related photography',
+        img:'https://i.ibb.co/4fqRs0Z/price-list13.jpg'
+    },
+    {
+        name: 'Interior Shooting',
+        price: 5000,
+        description: 'My interior photography have long reputation since I started working in this field. the package could be your best choice',
+        img:'https://i.ibb.co/mbmJrjJ/service-2.jpg'
+    },
+    {
+        name: 'Product Shooting',
+        price: 2000,
+        description: 'I am offering very affordable rate of product photography where I cam offering almost photos with $2000. I think I would be best deal for you',
+        img: 'https://i.ibb.co/jJmPTNX/HELLO.jpg'
+    },
+]

25de32794a476f53e9e3e45f8b2d80807c602e13
hasan-mohammad-bd <hasan.md.east@gmail.com>
2023-05-08 16:12:26 +0600

    adding mongodb connection

--- index.js
+++ index.js
@@ -12,8 +12,15 @@ app.use(cors());
 app.use(express.json());
 
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.xecwx.mongodb.net/?retryWrites=true&w=majority`;
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+const uri = "mongodb+srv://user1:0faSedldEFJcp4Zv@cluster0.xecwx.mongodb.net/?retryWrites=true&w=majority";
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
 
 const run = async()=>{
     try{
